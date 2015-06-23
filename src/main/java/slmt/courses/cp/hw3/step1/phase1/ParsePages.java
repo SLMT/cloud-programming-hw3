d72aa9c9c44420c1c81521df76f63b1552ae2403
slmt <sam123456777@gmail.com>
2015-06-24 00:22:45 +0800

    Init commit

---
+++ src/main/java/slmt/courses/cp/hw3/step1/phase1/ParsePages.java
@@ -0,0 +1,45 @@
+package slmt.courses.cp.hw3.step1.phase1;
+
+import java.io.IOException;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+
+public class ParsePages {
+	public static void main(String[] args) throws Exception {
+		ParsePages wc = new ParsePages();
+		wc.run(args[0], args[1]);
+	}
+
+	public void run(String inputPath, String outputPath) throws IOException {
+
+		// Create a job configuration
+		JobConf conf = new JobConf(ParsePages.class);
+		conf.setJobName("SLMT's Page Rank Job - Step 1 - Phase 1");
+
+		// Set up mapping
+		//conf.setMapperClass(BuildIndexMapper.class);
+		//conf.setMapOutputKeyClass(Text.class);
+		//conf.setMapOutputValueClass(TermFrequency.class);
+
+		// Set up partition
+		//conf.setPartitionerClass(BuildIndexPartitioner.class);
+
+		// Set up reducing
+		//conf.setReducerClass(BuildIndexReducer.class);
+		//conf.setOutputKeyClass(Text.class);
+		//conf.setOutputValueClass(TermFrequencyList.class);
+
+		FileInputFormat.addInputPath(conf, new Path(inputPath));
+		FileOutputFormat.setOutputPath(conf, new Path(outputPath));
+
+		conf.setNumMapTasks(3);
+		conf.setNumReduceTasks(2);
+
+		JobClient.runJob(conf);
+	}
+}
