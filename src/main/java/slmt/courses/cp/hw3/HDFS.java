4496843628ccb8c1462dae56d3dde3a2196b627a
slmt <sam123456777@gmail.com>
2015-06-30 01:07:37 +0800

    Finish the step 2

--- src/main/java/slmt/courses/cp/hw3/HDFS.java
+++ src/main/java/slmt/courses/cp/hw3/HDFS.java
@@ -7,11 +7,24 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
 public class HDFS {
+	
+	public static boolean mv(String oldPath, String newPath) {
+		try {
+			Path pathObj = new Path(oldPath);
+			FileSystem hdfs = pathObj.getFileSystem(new Configuration());
+			
+			return hdfs.rename(pathObj, new Path(newPath));
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 
-	public boolean rmdir(String path) {
+	public static boolean rmdir(String path) {
 		try {
 			Path pathObj = new Path(path);
 			FileSystem hdfs = pathObj.getFileSystem(new Configuration());
+			
 			return hdfs.delete(pathObj, true);
 		} catch (IOException e) {
 			e.printStackTrace();
--- src/main/java/slmt/courses/cp/hw3/step2/PageRankCalculator.java
+++ src/main/java/slmt/courses/cp/hw3/step2/PageRankCalculator.java
@@ -2,19 +2,30 @@ package slmt.courses.cp.hw3.step2;
 
 import java.io.IOException;
 
+import slmt.courses.cp.hw3.HDFS;
+
 public class PageRankCalculator {
-	
-	private static String TMP_DIR1 = "pageRankTmp1";
-	private static String TMP_DIR2 = "pageRankTmp2";
-	
+
+	private static String TMP_DIR = "pageRankTmp";
+
+	private static int INTERATION_TIMES = 15;
+
 	public static void main(String[] args) {
 		try {
-			
+			// In the first time, we use the first argument as the input
+			// directory
 			String input = args[0];
 			String output = args[1];
+			PageRankJob.run(input, output);
 			
-			new PageRankJob().run(input, output);
-			
+			// Then, we move the files from the output directory
+			// to the tmp. directory and use tmp. dir. as the input dir.
+			for (int i = 1; i < INTERATION_TIMES; i++) {
+				HDFS.rmdir(TMP_DIR);
+				HDFS.mv(output, TMP_DIR);
+				
+				PageRankJob.run(TMP_DIR, output);
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
